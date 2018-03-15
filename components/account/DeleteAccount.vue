e228606e5e7529da700775526e4b052123bf5e39
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-03-15 14:55:54 +0100

    Implemet #55: Ensure Delete Account Functionality Works

---
+++ components/account/DeleteAccount.vue
@@ -0,0 +1,27 @@
+
+<template lang="pug">
+.card
+  p Are you sure you want to delete your account?
+
+  button.button.danger.-has-icon.-small(type="button" v-on:click="deleteAccount")
+    span
+      i.fa.fa-trash-alt
+      | Delete Account
+</template>
+
+<script>
+export default {
+  methods: {
+    deleteAccount () {
+      this.$store.dispatch('deleteUser')
+        .then(() => {
+          this.$router.push('/')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
+
