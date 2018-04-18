1a942365eaedd16a0305ffa1377c8de22e586565
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-04-18 12:37:00 +0200

    Add two thank you pages #95

---
+++ pages/thank-you-monthly.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+ThankYou
+</template>
+
+<script>
+import ThankYou from '~/components/account/ThankYou'
+export default {
+  name: 'page-thank-you-monthly',
+
+  middleware: 'anonymous',
+
+  components: {
+    ThankYou
+  }
+}
+</script>
