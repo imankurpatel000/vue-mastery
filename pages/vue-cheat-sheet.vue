c710170895211c9a49d149525747f687642d111d
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-04-23 16:40:32 +0200

    Add vue-cheat-sheet page

---
+++ pages/vue-cheat-sheet.vue
@@ -0,0 +1,38 @@
+<template lang='pug'>
+  .homepage
+    .cheatsheet
+      CheatSheetMain
+    .community
+      CommunitySupport
+</template>
+
+<script>
+import CommunitySupport from '~/components/static/CommunitySupport'
+import CheatSheetMain from '~/components/static/CheatSheetMain'
+
+export default {
+  name: 'page-vue-cheat-cheet',
+
+  middleware: 'anonymous',
+
+  head () {
+    return {
+      title: 'Vue Mastery | Get the Ultimate Vue Cheat Sheet.',
+      meta: [{
+        hid: `og:url`,
+        property: 'og:url',
+        content: process.env
+      }]
+    }
+  },
+
+  components: {
+    CommunitySupport,
+    CheatSheetMain
+  }
+}
+</script>
+
+<style lang='stylus' scoped>
+
+</style>
