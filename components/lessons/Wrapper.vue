2e28592a75f39bd223504e7280f5ee16605aaf48
Pierre Macbook Air <schweiger.pierre@gmail.com>
2018-04-25 13:39:30 +0200

    Remove dowload link on paid content

--- components/lessons/Wrapper.vue
+++ components/lessons/Wrapper.vue
@@ -25,7 +25,7 @@ div
 
     aside.lesson-aside(v-if='!locked' v-cloak)
       .control-group
-        Download(:courseLink='current.downloadLink', :account='account')
+        Download(v-if='current.free' :courseLink='current.downloadLink', :account='account')
         SocialShare(:lesson='current' :baseUrl='baseUrl')
 
       .card.download(v-if='!isLesson' v-cloak)
