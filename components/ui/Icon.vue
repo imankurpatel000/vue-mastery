8ef668af7b2aa0820b73f86de86e1f8578f2363f
Dustin Delatore <dustin@delatore.me>
2018-06-15 11:45:51 +0100

    Add icon sprites for better icon creation

---
+++ components/ui/Icon.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  svg(class="icon" v-bind="$props")
+    use(v-bind="{'xlink:href':'/images/spr-'+type+'.svg#'+name}")
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    name: String,
+    type: {
+      type: String,
+      default: 'feather'
+    },
+    width: {
+      type: Number,
+      default: 24
+    },
+    height: {
+      type: Number,
+      default: 24
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.icon
+  &[type='feather']
+    stroke currentColor
+    stroke-width 2
+    stroke-linecap round
+    stroke-linejoin round
+    fill none
+  &[type='social']
+    fill currentColor
+</style>
