352d00195f87ccd8e30c50c1775f656ad58cf8cf
Dustin Delatore <dustin@delatore.me>
2018-11-19 17:08:59 -0500

    Add capitalize and updated time filter

--- plugins/filters.js
+++ plugins/filters.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import moment from 'moment'
 
 Vue.filter('time', (value) => {
   return value.replace(/^0(?:0:0?)?/, '')
+})
+
+Vue.filter('timeToText', (value) => {
+  let time = moment(value, 'HH:mm:ss').format('H[h] MM[min]')
+  return time.split(' ').filter(unit => parseInt(unit) !== 0).join(' ')
 })
 
 Vue.filter('pluralizeLesson', (value) => {
@@ -10,4 +16,13 @@ Vue.filter('pluralizeLesson', (value) => {
 
 Vue.filter('truncate', (text, stop, clamp) => {
   return text.slice(0, stop) + (stop < text.length ? clamp || '...' : '')
+})
+
+Vue.filter('capitalize', string => {
+  if (!string) return ''
+  return string
+    .trim()
+    .split(' ')
+    .map(word => word[0].toUpperCase() + word.slice(1))
+    .join(' ')
 })
