a40698f1024e474df6277ec90a3537c4b646aa10
Pierre Schweiger <pierre.schweiger@regily.com>
2018-11-20 18:48:01 +0200

    Add transition to annoucement bar

--- components/static/Announcement.vue
+++ components/static/Announcement.vue
@@ -1,14 +1,28 @@
 <template lang='pug'>
-  section.announcement-bar(v-if="!account || (account && !account.subscribed)")
-    p #[b() 35% Off] a year with Vue Mastery. Use code: #[b() BLACKFRIDAY] at checkout.
-    .anounce-icon
-      img(src='/images/ico-vue-master-yay.png')
+  transition(name="push" mode='out-in' appear)
+    section.announcement-bar(v-if="ready && !account || (account && !account.subscribed)" v-cloak)
+      p #[b() 35% Off] a year with Vue Mastery. Use code: #[b() BLACKFRIDAY] at checkout.
+      .anounce-icon
+        img(src='/images/ico-vue-master-yay.png')
 </template>
 
 <script>
 import { mapState } from 'vuex'
 export default {
   name: 'announcement-bar',
+
+  data () {
+    return {
+      ready: false
+    }
+  },
+
+  mounted () {
+    setTimeout(() => {
+      this.ready = true
+    }, 2000)
+  },
+
   computed: {
     ...mapState({
       account: result => result.account.account
@@ -32,6 +46,16 @@ export default {
   width 42px
   height 42px
   margin 0 18px
+
+
+.push-enter-active
+  transition margin-top ease-in .5s
+
+.push-leave-active
+  transition margin-top ease-out .5s
+
+.push-enter, .push-leave-to
+  margin-top: -90px
 
 </style>
 
