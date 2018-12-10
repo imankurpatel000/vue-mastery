a91de305003faa32da29a32d31cc1b60096c904a
Pierre Schweiger <pierre.schweiger@regily.com>
2018-12-10 16:30:20 +0200

    Add gift purchase thank you page

---
+++ pages/gift-purchase-thank-you.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+ClaimThankYou(:renewal="new Date() | dateFormat('MMMM D, YYYY', '1 year')")
+</template>
+
+<script>
+import ClaimThankYou from '~/components/account/ClaimThankYou'
+export default {
+  name: 'page-claim-thank-you-annual',
+
+  head () {
+    return {
+      title: 'Thank you | Vue Mastery'
+    }
+  },
+
+  middleware: 'anonymous',
+
+  components: {
+    ClaimThankYou
+  }
+}
+</script>
