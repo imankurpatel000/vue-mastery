798f955a82ce143ef4a6c082f30c20fefa5092af
Pierre Schweiger <pierre.schweiger@regily.com>
2019-01-22 17:19:36 -0300

    Start updating user settings to update email

--- components/account/SubscribeToMailingList.vue
+++ components/account/SubscribeToMailingList.vue
@@ -1,12 +1,32 @@
 <template lang='pug'>
 form.form.card
   .card-body
+    p.switch-legend I want to receive:
     .form-group.-switched
       .switch
         input(id='subscribeSwitch' type='checkbox' v-model='subscribedToMailingList' @change='subscribe')
         label(for='subscribeSwitch')
+      span Tips on using Vue Mastery Successfully
 
-      span I want to receive occasional emails about new educational content.
+    .form-group.-switched
+      .switch
+        input(id='subscribeBiweeklySwitch' type='checkbox' v-model='subscribedToBiweekly' @change='subscribe')
+        label(for='subscribeBiweeklySwitch')
+      span The BiWeekly Newsletter
+
+    .form-group.-switched
+      .switch
+        input(id='subscribeDiscountAndPromotionSwitch' type='checkbox' v-model='subscribedToDiscountAndPromotion' @change='subscribe')
+        label(for='subscribeDiscountAndPromotionSwitch')
+      span Information about Discounts and Promotions
+
+    .form-group.-switched
+      .switch
+        input(id='subscribeFreeContentSwitch' type='checkbox' v-model='subscribedToFreeContent' @change='subscribe')
+        label(for='subscribeFreeContentSwitch')
+      span New Free Content Announcements
+
+
 
     .form-error
       .callout.-error(v-if='formError.length > 0' v-text='formError')
@@ -38,19 +58,20 @@ export default {
       this.formSuccess = this.formError = ''
     },
 
-    subscribe () {
-      this.resetFormMessages()
-      clearTimeout(this.debounceTimer)
-      this.debounceTimer = setTimeout(() => {
-        this.$store.dispatch('userUpdateSubscription', this.subscribedToMailingList)
-          .then(() => {
-            this.formSuccess = 'Successfully updated your account details'
-          })
-          .catch((err) => {
-            this.formError = 'Error saving the profile changes'
-            console.error(err)
-          })
-      }, 500)
+    subscribe (e) {
+      console.log(e)
+      // this.resetFormMessages()
+      // clearTimeout(this.debounceTimer)
+      // this.debounceTimer = setTimeout(() => {
+      //   this.$store.dispatch('userUpdateSubscription', this.subscribedToMailingList)
+      //     .then(() => {
+      //       this.formSuccess = 'Successfully updated your account details'
+      //     })
+      //     .catch((err) => {
+      //       this.formError = 'Error saving the profile changes'
+      //       console.error(err)
+      //     })
+      // }, 500)
     }
   }
 }
@@ -61,4 +82,7 @@ export default {
   display flex
   flex-direction: row
   align-items center
+
+.switch-legend
+  margin-left 11px
 </style>
