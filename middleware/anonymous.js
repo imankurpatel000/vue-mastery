1bd28bc939884d93e1ff795899a1e0fd6f37ea8e
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-08-19 22:35:07 +0200

    Remove unused code

--- middleware/anonymous.js
+++ middleware/anonymous.js
@@ -1,12 +1,5 @@
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
-
 export default function ({
   isServer,
   store,
   redirect
-}) {
-  // if (!isServer && (store.getters.isAuthenticated && firebase.auth().currentUser)) {
-  //   return redirect('/account')
-  // }
-}
+}) {}
