b6d9d1fa062191012aca6cea0132c8d33eafc5b3
Ben Hong <bencodezen@gmail.com>
2019-08-23 16:54:31 -0400

    :sparkles: Add Ben to home page

--- components/static/MeetTeachers.vue
+++ components/static/MeetTeachers.vue
@@ -1,17 +1,22 @@
-<template lang='pug'>
+<template lang="pug">
   .banner
     .wrapper
       h2.title Meet Your Teachers
 
-      .gregg
+      .instructor.gregg
         img(src='/images/gregg-avatar.jpg')
         h3 Gregg Pollack
         p Gregg is passionate about teaching open source technology online. Previously he created Code School, an online software programming school which was acquired by Pluralsight.  He lives in Orlando, Florida with his two kids Ender and Alanna.
 
-      .adam
+      .instructor.adam
         img(src='/images/adam-avatar.jpg')
         h3 Adam Jahr
         p Adam Jahr is a developer and programming instructor passionate about making it easier to learn complex skills. He previously instructed at The Iron Yard’s coding bootcamp before joining Vue Mastery in order to help developers progress along their professional paths.
+
+      .instructor.ben
+        img(src='/images/ben-avatar.jpg')
+        h3 Ben Hong
+        p Ben Hong is a psychologist / developer / educator hybrid, whose origin story harkens back to the days when he would build cheesy websites like his “Fortune Cookie Universe” using one too many marque, blink elements, and dancing baby GIFs
 </template>
 
 <script>
@@ -20,7 +25,7 @@ export default {
 }
 </script>
 
-<style lang='stylus' scoped>
+<style lang="stylus" scoped>
 .wrapper
   padding-top ($vertical-space/2)
   padding-bottom ($vertical-space/2)
@@ -28,7 +33,8 @@ export default {
   grid-row-gap 2em
   grid-template-areas 'title'\
                       'gregg'\
-                      'adam'
+                      'adam'\
+                      'ben'
 
   +tablet-up()
     padding-top $vertical-space
@@ -37,7 +43,8 @@ export default {
     grid-row-gap 0
     grid-template-columns 1fr 1fr
     grid-template-areas 'title title'\
-                        'gregg adam'
+                        'gregg adam'\
+                        'ben ben'
 
 .title
   grid-area title
@@ -50,7 +57,7 @@ export default {
   +tablet-up()
     margin-bottom $vertical-space
 
-.gregg, .adam
+.instructor
   text-align center
   max-width 500px
   justify-self center
@@ -68,4 +75,7 @@ export default {
 .adam
   grid-area adam
 
+.ben
+  grid-area ben
+  margin-top ($vertical-space/2)
 </style>
