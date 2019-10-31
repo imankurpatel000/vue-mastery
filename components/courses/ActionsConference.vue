414f9085c65633f6e8e3c599550e2da550e7cb77
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-10-31 10:28:38 +0800

    Change coming soon label

--- components/courses/ActionsConference.vue
+++ components/courses/ActionsConference.vue
@@ -9,7 +9,7 @@
     div(v-if='isSubscribed()')
       span Subscribed
     div(v-else)
-      span Coming Soon
+      span Videos Coming Soon
 
       .button.primary.border.-full(@click.prevent='subscribedToMailingList()' v-if='account' v-cloak)
         | Notify Me
