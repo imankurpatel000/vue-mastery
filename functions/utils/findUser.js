dd84638b2f8ee4f9f1c69d3da9632be5d5634c17
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-11-05 23:26:02 +0800

    Add test function

---
+++ functions/utils/findUser.js
@@ -0,0 +1,24 @@
+const admin = require('firebase-admin')
+
+const serviceAccount = require('../../serviceAccountKey.json')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://vue-mastery.firebaseio.com'
+})
+
+const findEmail = (email) => {
+  return admin
+    .database()
+    .ref('accounts')
+    // .orderByChild('email')
+    // .orderByChild('chargebeeId')
+    .equalTo(email)
+    .once('child_added', (snapshot) => {
+      console.log(snapshot.val())
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+findEmail('schweiger.pierre@gmail.com')
