a0f2fc7a224d04ab9df3273236825a5501b79a82
Pierre Schweiger <schweiger.pierre@gmail.com>
2019-11-23 20:53:53 +0800

    Refine search request data

--- initial-search-request.js
+++ initial-search-request.js
@@ -1,75 +1,156 @@
+const flamelink = require('flamelink/app')
+require('flamelink/content')
+require('flamelink/storage')
+const admin = require('firebase-admin')
+const conf = require('./firebase')
+const serviceAccount = require(`./serviceAccountKey.json`)
 const algoliasearch = require('algoliasearch')
-const dotenv = require('dotenv')
-const firebase = require('firebase')
 const removeMd = require('remove-markdown')
 
-// load values from the .env file in this directory into process.env
-dotenv.config()
-
-// configure firebase
-firebase.initializeApp({
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-})
-const database = firebase.database()
+// Init firebase
+const firebaseConfig = {
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: conf.databaseURL,
+  storageBucket: conf.storageBucket
+}
+const firebaseApp = admin.initializeApp(firebaseConfig)
+const db = flamelink({ firebaseApp, isAdminApp: true, env: conf.env }).content
 
 // configure algolia
 const algolia = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY
+  conf.algolia.app_id,
+  conf.algolia.api_key
 )
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME)
 
-const strSplitOnLength = function (data, width) {
-  if (data.length <= 0) {
-    return [] // return an empty array
+const createIndexObject = function (data, url, category) {
+  let image
+
+  try {
+    image = data.image[0].url
+  } catch (error) {
+    console.log(`Image for the lesson ${data.title} does not exist`)
   }
 
-  var splitData = data.split(/([\s\n\r]+)/)
-  var arr = []
-  var cnt = 0
-  for (var i = 0; i < splitData.length; ++i) {
-    if (!arr[cnt]) arr[cnt] = ''  //Instantiate array entry to empty string if null or undefined
-
-    if (width < (splitData[i].length + arr[cnt].length)) cnt++
-
-    arr[cnt] += splitData[i]
+  return {
+    objectID: data.id,
+    url: url,
+    title: data.title,
+    slug: data.slug,
+    category: category,
+    date: data.date || data.presentedDate,
+    free: data.free || true,
+    description: data.description,
+    body: removeMd(data.markdown ? data.markdown : data.body, {
+      useImgAltText: true
+    }),
+    image: image
   }
-
-  return arr
 }
 
-// Get all lessons from Firebase
-database.ref('flamelink/environments/production/content/lessons/en-US').once('value', contacts => {
-  // Build an array of all records to push to Algolia
-  const records = []
-  contacts.forEach(contact => {
-    // get the key and data from the snapshot
-    const childKey = contact.key
-    const childData = contact.val()
-    // We set the Algolia objectID as the Firebase .key
-    childData.objectID = childKey
-
-    // Add object for indexing
-    records.push({
-      objectID: childData.objectID,
-      title: childData.title,
-      slug: childData.slug,
-      date: childData.date,
-      free: childData.free,
-      description: childData.description,
-      markdown: removeMd(childData.markdown)
+const getCoursesPage = async function () {
+  return db.get({
+    schemaKey: 'courses',
+    populate: [{
+      field: 'lessons',
+      subFields: [ 'lessons', 'image', 'status' ],
+      populate: [ 'image' ]
+    }, {
+      field: 'image',
+      subFields: [ 'image' ]
+    }]})
+    .then(async courses => {
+      const coursePages = []
+      for (const key of Object.keys(courses)) {
+        const course = courses[key]
+        if (course.hasOwnProperty('lessons')) {
+          for (const id of Object.keys(course.lessons)) {
+            const lesson = course.lessons[id]
+            if (lesson.status === 'published') {
+              coursePages.push(createIndexObject(lesson, `/courses/${course.slug}/${lesson.slug}`, 'course'))
+            }
+          }
+        }
+      }
+      return coursePages
     })
-  })
+}
 
-  // console.log(removeMd(records[0].markdown))
-  // Add or update new objects
-  index
-    .saveObjects(records)
+const getTalksPage = async function () {
+  return db.get({
+    schemaKey: 'conference',
+    populate: [{
+      field: 'talks',
+      subFields: [ 'lessons', 'image' ],
+      populate: [ 'image' ]
+    }, {
+      field: 'image',
+      subFields: [ 'image' ]
+    }]})
+    .then(async conferences => {
+      const talkPages = []
+      for (const key of Object.keys(conferences)) {
+        const conference = conferences[key]
+        if (conference.hasOwnProperty('talks')) {
+          // talkPages.push(createIndexObject(conference, `/conferences/${conference.slug}`, 'conferences'))
+          for (const id of Object.keys(conference.talks)) {
+            const talk = conference.talks[id]
+            if (talk.isVideoLive === 'true') {
+              talkPages.push(createIndexObject(conference, `/conferences/${conference.slug}/${talk.slug}`, 'conference'))
+            }
+          }
+        }
+      }
+      return talkPages
+    })
+}
+
+const getPostsPage = async function () {
+  return db.get({
+    schemaKey: 'posts',
+    populate: [{
+      field: 'image',
+      subFields: [ 'image' ]
+    }]})
+    .then(async posts => {
+      const postsPages = []
+      for (const key of Object.keys(posts)) {
+        const post = posts[key]
+        if (post.status === 'published') {
+          postsPages.push(createIndexObject(post, `/blog/${post.slug}`, 'blog'))
+        }
+      }
+      return postsPages
+    })
+}
+
+const saveToAlgolia = async function (type, pages) {
+  return algolia.initIndex(type)
+    .saveObjects(pages)
     .then(() => {
-      console.log('Lessons imported into Algolia')
+      console.log(`Content ${type} imported into Algolia`)
     })
     .catch(error => {
       console.error('Error when importing lesson into Algolia', error)
-      process.exit(1)
     })
-})
+}
+
+const indexAllContent = async function () {
+  console.log('Find all content for index')
+
+  const coursesPages = await getCoursesPage(db)
+  const talksPages = await getTalksPage(db)
+  const postsPages = await getPostsPage(db)
+
+  console.log(coursesPages.length,
+    talksPages.length,
+    postsPages.length)
+
+  // Get all lessons from Firebase
+  await saveToAlgolia('lessons', coursesPages)
+  await saveToAlgolia('talks', talksPages)
+  await saveToAlgolia('posts', postsPages)
+
+  process.exit(1)
+}
+
+indexAllContent()
