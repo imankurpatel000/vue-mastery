6ef642a04365fb82f6b625656af94ddeaa9a9e28
Pierre Schweiger <schweiger.pierre@gmail.com>
2020-04-12 17:45:01 +0700

    Fix module

--- nuxt.config.js
+++ nuxt.config.js
@@ -385,7 +385,7 @@ module.exports = {
         })
       }
     },
-    transpile: ['vue-instantsearch', 'instantsearch.js/es', 'vue-countup-v2', 'countup.js']
+    transpile: ['vue-instantsearch', 'instantsearch.js/es', 'vue-countup-v2', 'countup.js', 'vue-agile']
   },
   // Extend route for course path
   router: {
